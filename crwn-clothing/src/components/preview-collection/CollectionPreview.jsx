3502b77216cc935640b63a3bb34e0cbc2ff0d438
Michael Bui <michaelbui99@hotmail.com>
2021-10-28 01:46:12 +0200

    CollectionItem component with styling, which is rendered in CollectionPreview

--- crwn-clothing/src/components/preview-collection/CollectionPreview.jsx
+++ crwn-clothing/src/components/preview-collection/CollectionPreview.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import CollectionItem from "../collection-item/CollectionItem";
 import "./collection-preview.styles.scss";
+
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
@@ -7,8 +9,8 @@ const CollectionPreview = ({ title, items }) => {
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <div key={item.id}>{item.name}</div>
+          .map(({ id, ...otherCollectionProps }) => (
+            <CollectionItem key={id} {...otherCollectionProps} />
           ))}
       </div>
     </div>
